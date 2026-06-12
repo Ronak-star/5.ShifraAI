170a8b7692fffab054f99cff961f5492bc4b58a1
Ronak Tailor <tailorronak6@gmail.com>
2026-06-12 16:31:11 +0530

    Update CLIENT_URL to production login URL

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,7 +9,7 @@ import Builder from './pages/Builder'
 import Billing from './pages/Billing'
 import { Toaster } from "react-hot-toast"
 export const ServerUrl = "https://5-shifra-ai.vercel.app/"
-export const CLIENT_URL = "http://localhost:5173"
+export const CLIENT_URL = "https://5-shifra-ai-55fv.vercel.app/login"
 function App() {
   const [user, setUser] = useState(null)
   const [loading, setLoading] = useState(true)
